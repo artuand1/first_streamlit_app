cd17a5641141738bd8987db47389ad183521057e
artuand1 <97694587+artuand1@users.noreply.github.com>
2022-11-24 11:39:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast menu')
-streamlit.text('kale, spinach and smoothie')
-streamlit.text('eggs with bacon')
-streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('🥗kale, spinach and smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 and blueberry oatmeal')
+streamlit.text('🍞Avocado toast')
 
 

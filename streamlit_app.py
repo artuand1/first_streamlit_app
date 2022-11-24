ee2124ef1dd14115910fd6bb5742a02d36057ebb
artuand1 <97694587+artuand1@users.noreply.github.com>
2022-11-24 11:33:40 +0100

    Update streamlit_app.py
    added headed breakfast and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+streamli.header('Breakfast menu')
+streamlit.text('kale, spinach and smoothie')
+streamlit.text('eggs with bacon')
+streamlit.text('Omega 3 and blueberry oatmeal')
+
+

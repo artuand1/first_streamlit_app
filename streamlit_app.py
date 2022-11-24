f3e7d4c6bea7ef6836c69e7f31cd0ad9de3a76ce
artuand1 <97694587+artuand1@users.noreply.github.com>
2022-11-24 11:40:07 +0100

    Update streamlit_app.py
    header special menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.text('🥗kale, spinach and smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥣Omega 3 and blueberry oatmeal')
 streamlit.text('🍞Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 

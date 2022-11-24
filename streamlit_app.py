53c123cce7995c8907cf2fe4afd09e70d196a0be
artuand1 <97694587+artuand1@users.noreply.github.com>
2022-11-24 11:34:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
-streamli.header('Breakfast menu')
+streamlit.header('Breakfast menu')
 streamlit.text('kale, spinach and smoothie')
 streamlit.text('eggs with bacon')
 streamlit.text('Omega 3 and blueberry oatmeal')
